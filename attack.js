30462a645952510be43f13d724f9b7f85a276e84
fullaware <>
2018-03-23 06:52:50 -0400

    cleanliness

--- attack.js
+++ attack.js
@@ -8,13 +8,13 @@ var hp = {v1:0,v2:0};
 
 function hp1(hp){
     hp.v1++;
-    getRandomInt(1,4000)
+    getRandomInt(1,4000);
     return hp;
 }
 
 function hp2(hp){
     hp.v2++;
-    getRandomInt(1,4000)
+    getRandomInt(1,4000);
     return hp;
 }
 
