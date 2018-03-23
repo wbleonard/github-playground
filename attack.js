d253321bb00363f46fef27eac8ee0b991c447a44
fullaware <>
2018-03-23 06:47:33 -0400

    syntax cleanup

--- attack.js
+++ attack.js
@@ -1,12 +1,12 @@
 // function to overcome decimal math
 
 function add_decimals(x,y){
-    return Math.round((x + y) * 1e12) / 1e12
+    return Math.round((x + y) * 1e12) / 1e12;
 }
 
-var num = 2/41.882
+var num = 2/41.882;
 var n = num.toFixed(2);
-console.log(add_decimals(.01,.06))
+console.log(add_decimals(.01,.06));
 //  prints 0.07
 
 function getRandomInt(min, max) {
@@ -15,18 +15,18 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-var hp = {v1:0,v2:0}
+var hp = {v1:0,v2:0};
 
 function hp1(hp){
-    hp.v1++
+    hp.v1++;
     getRandomInt(1,4000)
-    return hp
+    return hp;
 }
 
 function hp2(hp){
-    hp.v2++
+    hp.v2++;
     getRandomInt(1,4000)
-    return hp
+    return hp;
 }
 
 function fight(){
@@ -36,6 +36,8 @@ function fight(){
     }
 }
 
-fight()
+fight();
 
-console.log(hp)
+console.log(hp);
+
+
